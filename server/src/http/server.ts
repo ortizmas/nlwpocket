236265563dc13d83baf2c034fe7c44b3d417603a
Eber Ortiz <ortizmas14@gmail.com>
2024-09-11 09:24:42 -0300

    Implementing pending goals

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -2,6 +2,7 @@ import { fastify } from "fastify";
 import { serializerCompiler, validatorCompiler, ZodTypeProvider } from "fastify-type-provider-zod";
 import { request } from "http";
 import { createGoal } from "../functions/create-goal";
+import { getWeekPendingGoals } from '../functions/get-week-pending-goals'
 import z from "zod";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
@@ -24,6 +25,12 @@ app.post('/goals', {
             desiredWeeklyFrequency
         })
     })
+
+app.get('/pending-goals', async () => {
+    const { pendingGoals } = await getWeekPendingGoals()
+
+    return { pendingGoals }
+})
 
 app.listen({
     port: 3333,
